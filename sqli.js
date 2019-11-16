f44f0e82fe15d968a39c8b29fad5f869374834af
Ryan Medlyn <ryan.medlyn@gmail.com>
2019-11-15 18:22:27 -0700

    add sqli function outlines

---
+++ sqli.js
@@ -0,0 +1,39 @@
+function midQueryComment(query) {
+    // TODO: code this
+}
+
+function tautology(query) {
+    // TODO: code this
+}
+
+function illegal(query) {
+    // TODO: code this
+}
+
+function union(query) {
+    // TODO: code this
+}
+
+function piggyback(query) {
+    // TODO: code this
+}
+
+function inference(query) {
+    // TODO: code this
+}
+
+function altEncoding(query) {
+    // TODO: code this
+}
+
+function process() {
+    let query = document.getElementById("query").value;
+    let results = {
+        //TODO: call all query functions
+    }
+}
+
+function score(results) {
+    // TODO: generate score
+    // TODO: send to #results
+}
