1a7725da03fbe4d65f24029e65714f7573bee602
Michael A. Warren <mawarren24@gmail.com>
2019-11-30 11:39:36 -0700

    Dev Checkpoint 201911261318 (#2)

--- sqli.js
+++ sqli.js
@@ -2,16 +2,52 @@ function midQueryComment(query) {
     // TODO: code this
 }
 
-function tautology(query) {
-    // TODO: code this
+function tautology(query) {     
+    let where = query.search(/where/i);
+
+    if (where != null) {
+        let sub1 = query.substring(where + 6);
+
+        let or = sub1.matchAll(/or/i);
+        
+        if (or[0] != null) {
+            for (let i = 0; i < or.length; i++) {
+                let sub2 = sub1.substring(or[i] + 3);
+                let test = sub2.indexOf("=");
+                
+                let lhs = sub2.substring(0,test);
+                let rhs = sub2.substring(test+1,2*test+1);
+
+                if (lhs == rhs) {
+                    return {"Assert True": true};
+                }
+            }
+        }
+
+        return {"Assert True": false};
+    }
 }
 
-function illegal(query) {
-    // TODO: code this
+function illegal(query) { 
+    let illegalInstance = query.search(/convert/i);
+
+    if (illegalInstance != null) {
+        return {convert: true};
+    }
+    else {
+        return {convert: false};
+    }
 }
 
 function union(query) {
-    // TODO: code this
+    let unionInstance = query.search(/union select/i);
+
+    if (unionInstance != null) {
+        return {'UNION SELECT': true};
+    }
+    else {
+        return {'UNION SELECT': false};
+    }
 }
 
 function piggyback(query) {
@@ -29,8 +65,16 @@ function altEncoding(query) {
 function process() {
     let query = document.getElementById("query").value;
     let results = {
-        //TODO: call all query functions
-    }
+        midQueryComment: midQueryComment(query),
+        tautology: tautology(query),
+        illegal: illegal(query),
+        union: union(query),
+        piggyback: piggyback(query),
+        inference: inference(query),
+        altEncoding: altEncoding(query)
+    };
+
+    score(results);
 }
 
 function score(results) {
